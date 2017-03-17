cd02356c8ca3a22874ae52f7a259609847bc685e
Matthew Wells <matthew@ergon.tech>
2017-03-17 13:54:44 -0700

    I think it works now...?

--- PostInstallHandler.php
+++ PostInstallHandler.php
@@ -3,37 +3,66 @@
 namespace ErgonTech\ModuleGenerator;
 
 use Composer\Script\Event;
+use Mpw\MageScaffold\ModuleScaffolder;
+use Phly\Mustache\Resolver\AggregateResolver;
+use Phly\Mustache\Resolver\DefaultResolver;
 
 class PostInstallHandler
 {
-    private $codePools = ['community', 'local'];
+    /**
+     * @var ModuleScaffolder
+     */
+    private static $moduleScaffolder;
 
-    public function promptForModuleInformation(Event $event)
+    public static function getModuleScaffolder()
+    {
+        if (!isset(static::$moduleScaffolder)) {
+            $res = new AggregateResolver();
+            $defaultResolver = new DefaultResolver();
+            $defaultResolver->addTemplatePath(__DIR__ . '/vendor/mpw/mage-scaffold/templates');
+            $res->attach($defaultResolver);
+
+            static::$moduleScaffolder = new \Mpw\MageScaffold\ModuleScaffolder(
+                new \Mpw\MageScaffold\FileScaffolder(
+                    new \Phly\Mustache\Mustache($res),
+                    new \League\Flysystem\Filesystem(new \League\Flysystem\Adapter\Local('./tmp'))));
+        }
+
+        return static::$moduleScaffolder;
+    }
+
+    public static function setModuleScaffolder(ModuleScaffolder $moduleScaffolder)
+    {
+        static::$moduleScaffolder = $moduleScaffolder;
+    }
+
+    public static function promptForModuleInformation(Event $event)
     {
         $io = $event->getIO();
 
-        $moduleName = $io->ask('Enter Module name');
-        $codePool = $io->select('Choose Code Pool', $this->codePools, 'community');
+        $moduleName = $io->ask('Enter Module Name: ');
+        $isCommunity = $io->askConfirmation('Community Code Pool [<comment>yes</comment>]? ', true);
+        $version = $io->ask('Enter Module Version: [<comment>0.1.0</comment>] ', '0.1.0');
 
-        $this->guardAgainstInvalidModuleNames($moduleName);
-        $this->guardAgainstInvalidCodePool($codePool);
+        static::guardAgainstInvalidModuleNames($moduleName);
+
+        static::getModuleScaffolder()->generate($moduleName, $isCommunity, true, $version);
     }
 
-    private function guardAgainstInvalidModuleNames($name)
+    private static function guardAgainstInvalidModuleNames($name)
     {
         if (substr_count($name, '_') !== 1) {
             throw new ModuleNameException('A valid module name should have the following format: VendorName_ModuleName');
         }
     }
 
-    /**
-     * @param $codePool
-     * @throws CodePoolChoiceException
-     */
-    private function guardAgainstInvalidCodePool($codePool)
+    public static function validateModuleName($moduleName)
     {
-        if (!in_array($codePool, $this->codePools)) {
-            throw new CodePoolChoiceException('The codepool must be one of the displayed options');
-        }
+        return substr_count($moduleName, '_') === 1;
+    }
+
+    public static function validateModuleVersion($version)
+    {
+        return version_compare($version, '0.0.0.0', '>=');
     }
 }
--- test/PostInstallHandlerTest.php
+++ test/PostInstallHandlerTest.php
@@ -12,9 +12,8 @@ namespace test\ErgonTech\ModuleGenerator;
 use Composer\IO\IOInterface;
 use Composer\IO\NullIO;
 use Composer\Script\Event;
-use ErgonTech\ModuleGenerator\CodePoolChoiceException;
-use ErgonTech\ModuleGenerator\ModuleNameException;
 use ErgonTech\ModuleGenerator\PostInstallHandler;
+use Mpw\MageScaffold\ModuleScaffolder;
 
 
 class PostInstallHandlerTest extends \PHPUnit_Framework_TestCase
@@ -29,6 +28,11 @@ class PostInstallHandlerTest extends \PHPUnit_Framework_TestCase
      */
     private $io;
 
+    /**
+     * @var ModuleScaffolder|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $moduleScaffolder;
+
     protected function setUp()
     {
         parent::setUp();
@@ -41,59 +45,62 @@ class PostInstallHandlerTest extends \PHPUnit_Framework_TestCase
 
         $this->io = $this->getMockBuilder(NullIO::class)
             ->setMethods([
-                'ask', 'select'
+                'ask', 'askConfirmation'
             ])
             ->getMock();
 
         $this->event->expects(static::any())
             ->method('getIo')
             ->willReturn($this->io);
+
+        $this->moduleScaffolder = $this->getMockBuilder(ModuleScaffolder::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'generate'
+            ])
+            ->getMock();
+
+        PostInstallHandler::setModuleScaffolder($this->moduleScaffolder);
     }
 
-    public function testInvalidModuleName()
+    public function testModuleNameValidation()
     {
-        $this->io->expects(static::once())
-            ->method('ask')
-            ->with('Enter Module name')
-            ->willReturn('asdf'); // <-- that's not a valid module name!
-
-        $subject = new PostInstallHandler();
-
-        $this->setExpectedException(ModuleNameException::class);
-        $subject->promptForModuleInformation($this->event);
+        static::assertFalse(PostInstallHandler::validateModuleName('asdf'));
+        static::assertTrue(PostInstallHandler::validateModuleName('Asdf_Asdf'));
     }
 
-    public function testInvalidCodePool()
+    public function testVersionStringValidation()
     {
-        $this->io->expects(static::once())
-            ->method('ask')
-            ->with('Enter Module name')
-            ->willReturn('Asdf_Asdf');
-
-        $this->io->expects(static::once())
-            ->method('select')
-            ->with('Choose Code Pool')
-            ->willReturn('blah');
-
-
-        $subject = new PostInstallHandler();
-        $this->setExpectedException(CodePoolChoiceException::class);
-        $subject->promptForModuleInformation($this->event);
+        static::assertFalse(PostInstallHandler::validateModuleVersion('asdf'));
+        static::assertTrue(PostInstallHandler::validateModuleVersion('0.1.0'));
+        static::assertTrue(PostInstallHandler::validateModuleVersion('1.0'));
+        static::assertTrue(PostInstallHandler::validateModuleVersion('1'));
     }
 
     public function testValidInput()
     {
+
         $this->io->expects(static::any())
             ->method('ask')
-            ->with('Enter Module name')
-            ->willReturn('Asdf_Asdf');
+            ->withConsecutive(['Enter Module Name: '], ['Enter Module Version: [<comment>0.1.0</comment>] ', '0.1.0'])
+            ->willReturnMap([
+                ['Enter Module Name: ', null, 'Asdf_Asdf'],
+                ['Enter Module Version: [<comment>0.1.0</comment>] ', '0.1.0', '0.1.0']
+            ]);
 
         $this->io->expects(static::once())
-            ->method('select')
-            ->with('Choose Code Pool')
-            ->willReturn('community');
+            ->method('askConfirmation')
+            ->with('Community Code Pool [<comment>yes</comment>]? ', true)
+            ->willReturn(true);
 
-        $subject = new PostInstallHandler();
-        $subject->promptForModuleInformation($this->event);
+        $this->moduleScaffolder->expects(static::once())
+            ->method('generate')
+            ->with(
+                'Asdf_Asdf',
+                true,
+                true,
+                '0.1.0');
+
+        PostInstallHandler::promptForModuleInformation($this->event);
     }
 }
